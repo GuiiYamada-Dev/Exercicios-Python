8014f84e1f74c870f18f53c8bdd5160fd0678247
Guilherme Valério <gui_marcani@hotmail.com>
2025-11-21 16:11:09 -0300

    Lendo e somando dois numeros

--- Exercicios/Ex003.py
+++ Exercicios/Ex003.py
@@ -2,3 +2,5 @@ n1 = int(input("Digite um numero: "))
 n2 = int(input("Digite outro numero: "))
 soma = (n1 + n2)
 print(f"A soma entre o numero {n1} e o numero {n2} foi: {soma}")
+
+#Programa no qual pegamos 2 numeros, e logo em seguida fazemos a soma! 
