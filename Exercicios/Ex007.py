ce507f1ec3aebaa435f9ba3144becc5c306eee76
Guilherme Valério <gui_marcani@hotmail.com>
2025-11-21 16:37:35 -0300

    Média Aritmética

--- Exercicios/Ex007.py
+++ Exercicios/Ex007.py
@@ -2,3 +2,5 @@ n1 = float(input("Digite sua primeira nota: "))
 n2 = float(input("Digite sua segunda nota: "))
 media = (n1 + n2) / 2
 print(f"A sua média entre {n1} e {n2} foi igual a {media}")
+
+#Programa no qual o usuario fornece duas notas, e então é retornado o valor de sua média! 
