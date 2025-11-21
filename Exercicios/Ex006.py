eb2a8e24f27d51fc382548ed3f8bf0c57916e55a
Guilherme Valério <gui_marcani@hotmail.com>
2025-11-21 16:33:56 -0300

    Dobro, Triplo, Raiz Quadrada

--- Exercicios/Ex006.py
+++ Exercicios/Ex006.py
@@ -3,3 +3,5 @@ dobro = numero * 2
 triplo = numero * 3
 raiz = numero ** (1/2)
 print(f"O dobro de {numero} vale {dobro}. \nO triplo de {numero} vale {triplo}. \nA raiz quadrada de {numero} é igual a {raiz:.2f}")
+
+#Programa no qual pedimos um numero ao usuario e em seguida obtemos o resultado do seu dobro, triplo e raiz quadrada!
