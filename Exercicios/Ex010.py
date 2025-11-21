5672259d12bc0ffd8333cea13f54e5a19db84ccd
Guilherme Valério <gui_marcani@hotmail.com>
2025-11-21 16:49:25 -0300

    Real para Dolar

--- Exercicios/Ex010.py
+++ Exercicios/Ex010.py
@@ -1,3 +1,5 @@
 dinheiro = float(input("Quanto reais você tem para converter? R$ "))
 dolar = dinheiro / 3.27
 print(f"Com R${dinheiro:.2f}, você consegue o equivalente a US${dolar:.2f}!")
+
+# Programa no qual o usuario digita a quantidade de Reais que deseja converter para Dolares! (ps. não é o Dolar atual, alias saudades de quando o dolar era tão baixo assim.)
