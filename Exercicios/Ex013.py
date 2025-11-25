c827c8900ec4650b7067d2cdcd8af2645d6cbc9d
Guilherme Valério <gui_marcani@hotmail.com>
2025-11-25 16:55:25 -0300

    Reajuste salarial

---
+++ Exercicios/Ex013.py
@@ -0,0 +1,6 @@
+salario = float(input("Digite seu salario: "))
+ajuste = salario + (salario * 15 / 100)
+print(f"Devido ao seu desempenho o seu salario de R${salario} sofreu um ajuste, e agora você ira receber um aumento!!")
+print(f"O seu novo salario é no valor de {ajuste}")
+
+#Programa no qual calculamos o aumento salarial de um trabalhor em 15% com base no seu recebimento atual fornecido pelo usuario!
