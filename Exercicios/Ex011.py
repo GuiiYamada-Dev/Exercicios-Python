d7af9939827321c3a459d492e24aa0468da974c0
Guilherme Valério <gui_marcani@hotmail.com>
2025-11-25 16:37:28 -0300

    Largura e Altura

---
+++ Exercicios/Ex011.py
@@ -0,0 +1,8 @@
+lar = float(input("Largura da parede: "))
+alt = float(input("Altura da parede: "))
+area = lar * alt
+print(f"Sua parede tem a dimensão de {lar}x{alt} e sua area é de {area}m².")
+tinta = area / 2
+print(f"Para pintar essa parede, você precisará de {tinta}l de tinta.")
+
+# Programa no qual pedimos ao usuario a largura e altura de uma parede em metros e então nosso programa nos nos devolve o valor da area e a quantidade de tinta necessaria para pinta-la! 
