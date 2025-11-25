82a3d8c2c15c1e58432702fd59f4c8335e4e9b7c
Guilherme Valério <gui_marcani@hotmail.com>
2025-11-25 16:45:58 -0300

    Calculando Descontos

---
+++ Exercicios/Ex012.py
@@ -0,0 +1,6 @@
+produto = float(input("Qual o valor do produto? "))
+desconto = produto * 5 / 100
+print (f"O seu produto de R${produto} recebeu um desconto de 5%! então agora você ira pagar apenas R${produto - desconto}!")
+print(f"PS. o senhor recebeu um desconto de R${desconto}")
+
+#Programa no qual pedimos ao usuario o valor para que então calculemos o desconto de 5% em cima do valor de seu produto informado!
