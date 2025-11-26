252bb64df7e98d9812bb1331386486f6a7078f26
Guilherme Valério <gui_marcani@hotmail.com>
2025-11-26 15:13:19 -0300

    Conversor de temperaturas

---
+++ Exercicios/Ex014.py
@@ -0,0 +1,5 @@
+c = float(input("Qual a temperatura em °C? "))
+f = c * 1.8 + 32
+print(f"Com uma temperatura de {c}°C em fahrenheit você tem {f}°F!")
+
+#Programa no qual convertemos celsius para fahrenheit!
