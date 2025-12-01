0bc3f5e27824109b87f5b29c7910b7985f829526
Guilherme Valério <gui_marcani@hotmail.com>
2025-12-01 16:01:23 -0300

    Quebrando um numero

---
+++ Exercicios/Ex016.py
@@ -0,0 +1,7 @@
+num = float(input("Digite um valor real: "))
+print(f"O valor digitado foi {num} e a sua porção inteira é {int(num)}.")
+
+#Programa no qual pedimos ao usuário um valor real e, em seguida, mostramos este número e exibimos sua porção inteira, usando a função int().
+
+#Também é possível utilizar a função trunc() importando a biblioteca math.
+#Porém, trunc() apenas realiza o truncamento (remover os decimais), enquanto int() faz isso e também realiza conversão de tipo.
