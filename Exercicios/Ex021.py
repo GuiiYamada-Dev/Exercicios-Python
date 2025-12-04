1a44ae892a3de9c454827b949d8bfb068224c024
Guilherme Valério <gui_marcani@hotmail.com>
2025-12-04 17:00:54 -0300

    Tocando um mp3

--- Exercicios/Ex021.py
+++ Exercicios/Ex021.py
@@ -1,1 +1,11 @@
 import pygame 
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("ex021song.mp3")
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+
+
+# Programa que toca uma música usando a biblioteca pygame.
+# Inicializamos o pygame e o mixer de áudio, carregamos o arquivo MP3 e mantemos o programa rodando até a música terminar de tocar.
