d0dd9c832124a2c8874f1ff400abe2c388c3fd7c
Guilherme Valério <gui_marcani@hotmail.com>
2025-12-04 18:33:09 -0300

    Verificando as primeiras letras de um texto

---
+++ Exercicios/Ex024.py
@@ -0,0 +1,4 @@
+cidade = str(input("Digite o nome de uma cidade que comece com Santo: ")).strip()
+print(cidade[:5].upper() == "SANTO")
+
+#Programa no qual pedimos ao usuario para digitar uma cidade que comece com "Santo" e então retornamos se a cidade digitada começa (True) ou não começa (False) com Santo. 
