da152b3f65004a4f9f1ed28fc7d56ba707cd1272
Guilherme Valério <gui_marcani@hotmail.com>
2025-12-04 22:50:26 -0300

    Procurando uma string dentro de outra

---
+++ Exercicios/Ex025.py
@@ -0,0 +1,4 @@
+nome = str(input("Qual é o seu nome completo? ")).strip()
+print(f"Seu nome tem Silva? {"silva" in nome.lower()}")
+
+#Programa no qual pedimos ao usuario que digite seu nome completo e então mostramos na tela se o nome possui o sobrenome 'Silva'.
