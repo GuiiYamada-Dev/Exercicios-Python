b873f072f2912fb0354157cd477e4ec17ee14a0a
Guilherme Valério <gui_marcani@hotmail.com>
2025-12-11 16:26:27 -0300

    Par ou Impar

---
+++ Exercicios/Ex030.py
@@ -0,0 +1,7 @@
+num = int(input("Digite um numero: "))
+if num % 2 == 1:
+    print(f"O numero {num} é IMPAR")
+else: 
+    print(f"O numero {num} é PAR")
+
+#programa no qual pedimos ao usuario que digite um numero e então retornamos se o numero é par ou impar!
