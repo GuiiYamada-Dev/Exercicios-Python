7e119f15f80955a705fed278b772fe80b7993fe3
Guilherme Valério <gui_marcani@hotmail.com>
2025-12-16 16:07:40 -0300

    Comparando numeros

---
+++ Exercicios/Ex038.py
@@ -0,0 +1,10 @@
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+if n1 > n2:
+    print("O PRIMEIRO valor é maior")
+elif n1 < n2:
+    print("O SEGUNDO valor é maior")
+else:
+    print("Os valores são IGUAIS!")
+
+#Programa simples que informa qual valor digitado foi maior, o primeiro, o segundo, ou se ambos são iguais!
