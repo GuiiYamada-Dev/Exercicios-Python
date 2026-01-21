825bc2c409e557dc82b8d49d857bfc104cb277db
Guilherme Valério <gui_marcani@hotmail.com>
2026-01-21 16:00:54 -0300

    Contagem regressiva

---
+++ Exercicios/Ex046.py
@@ -0,0 +1,7 @@
+from time import sleep
+for cont in range(10, -1, -1):
+    print(cont)
+    sleep(0.5)
+print('BUM! BUM! POOOW!')
+
+#programa no qual usamos o for para fazer uma contagem regressiva juntamente do sleep para atrasar o tempo que cada um é mostrado na tela!
