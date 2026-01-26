fb6a79cfba2793651ab82af420e0f850e00cd665
Guilherme Valério <gui_marcani@hotmail.com>
2026-01-26 15:53:33 -0300

    Contagem de pares

---
+++ Exercicios/Ex047.py
@@ -0,0 +1,8 @@
+#for n in range(1, 51):
+#    if n % 2 == 0:
+#        print(n, end=' ')
+
+for m in range(2, 52, 2):
+    print(m, end=' ')
+
+#Programa no qual utilizando o for, mostramos na tela apenas os numeros pares!
