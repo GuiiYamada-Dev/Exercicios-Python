8d325fd3bbedeb8d23cef28be8c3c9147e10572e
Guilherme Valério <gui_marcani@hotmail.com>
2026-01-27 15:51:16 -0300

    Tabuada 2.0

---
+++ Exercicios/Ex049.py
@@ -0,0 +1,5 @@
+tab = int(input("Digite um numero para ver sua tabuada: "))
+for c in range(1, 11):
+    print(f'{tab} X {c:2} = {tab * c}')
+
+#Programa no qual facilitamos o exercicio 09, utilizando o FOR
