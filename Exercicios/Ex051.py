f2da43227f823081867652b828db7b37851f7abb
Guilherme Valério <gui_marcani@hotmail.com>
2026-02-10 16:37:12 -0300

    Update Ex051.py

--- Exercicios/Ex051.py
+++ Exercicios/Ex051.py
@@ -1,7 +1,7 @@
 termo = int (input("Primeiro termo: "))
 razão = int(input("Razão: "))
 décimo = termo + (10 - 1) * razão
-for c in range (termo, décimo, razão):
+for c in range (termo, décimo + razão, razão):
     print(f"{c} ", end= '⭢ ')
 print("ACABOU")
 
