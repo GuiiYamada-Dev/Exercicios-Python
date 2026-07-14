e54a3aa7d585097028923eee8faed2fd34359aed
Guilherme Valério <gui_marcani@hotmail.com>
2026-07-14 19:22:53 -0300

    Print Especial

---
+++ Exercicios/Ex097.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tamanho = len(msg) + 3
+    print('~' * tamanho)
+    print(f' {msg}')
+    print('~' * tamanho)
+
+escreva('Guilherme Valerio')
+escreva('Futuro Fundador Das Empresas GM')
+escreva('GM')
+
+#programa no qual usando funções usamos um print que se adapta ao tamanho das mensagens! 
