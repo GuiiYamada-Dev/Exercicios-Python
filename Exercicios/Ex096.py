4e32adfa3f57c5937a5d1c85cff75cd9d0ee303b
Guilherme Valério <gui_marcani@hotmail.com>
2026-07-14 18:01:35 -0300

    Função que calcula área

---
+++ Exercicios/Ex096.py
@@ -0,0 +1,8 @@
+def area(largura, comprimento):
+    area_total = largura * comprimento
+    print(f'A area do terreno {largura}x{comprimento} é {area_total}m²')
+l = float(input("Largura (m): "))
+c = float(input("Comprimento (m): "))
+area(l, c)
+
+#Programa simples utilizando funções para calcular uma Área! 
